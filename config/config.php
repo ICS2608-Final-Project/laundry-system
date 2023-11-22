af3594dbd34f87b6237864c7d72f10ff1a56940d
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-11-22 22:10:26 +0800

    refactored project structure

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define($HOST, "localhost");
+define($PORT, 3306);
+define($SOCKET, "");
+define($USER, "root");
+define($PASSWORD, "kody#181118");
+define($DB_NAME, "");
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    $page_title = "Home Page";
+    $page_description = "";
+    require_once "template/header.php";
+?>
