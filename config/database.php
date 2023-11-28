a7d1fed2989d5905ffefbc42dfbe062d9c59d36f
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-11-28 10:36:55 +0800

    modified config

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_SOCKET', '');
+// Place our database username here
 define('DB_USERNAME', 'root');
+// Place our database password here
 define('DB_PASSWORD', '');
-define('DB_NAME', 'practice_db');
+// Place our database name here
+define('DB_NAME', '');
