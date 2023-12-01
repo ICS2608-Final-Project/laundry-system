be49344e51996cb8d48011e7e97df4aa0e76559c
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-01 17:54:18 +0800

    added creation of schema

--- db.sql
+++ db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `lovehon_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lovehon_db`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: lovehon_db
@@ -153,4 +155,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-01 17:52:05
+-- Dump completed on 2023-12-01 17:53:20
