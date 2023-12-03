cfdc213b7df1de8db926bebf01a657f35ca7ea2a
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-03 13:00:16 +0800

    added a hash password function for admin

--- admin/loginHandler.php
+++ admin/loginHandler.php
@@ -5,7 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = sanitizeInput($_POST['password']);
     if (empty($username)) {
         echo "username is empty";
-
     }
     if (empty($password)) {
         echo "password is empty";
@@ -25,4 +24,11 @@ function is_username_empty($username) {
 
 function is_password_empty($password) {
     return empty($password);
+}
+
+function hash_password(string $password) {
+    $options = [
+        'cost' => 12
+    ];
+    return password_hash($password, PASSWORD_BCRYPT, $options);
 }
