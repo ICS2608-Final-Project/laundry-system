15c2828902108fb6fb756a93b4372470dae124bb
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-03 12:07:03 +0800

    added login handler

--- admin/index.php
+++ admin/index.php
@@ -3,22 +3,22 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Love Hon Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body class="text-bg-dark min-vh-100 d-flex justify-content-center align-items-center">
     <div class="rounded border border-secondary border-opacity-25 p-4 w-25">
         <h3>Admin Login</h3>
-        <form action="" method="post">
+        <form action="loginHandler.php" method="post">
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="username" class="form-label">Username</label>
+                <input type="username" class="form-control" id="username" name="username">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
---
+++ admin/loginHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = sanitizeInput($_POST['username']);
+    $password = sanitizeInput($_POST['password']);
+    if (empty($username)) {
+        echo "username is empty";
+
+    }
+    if (empty($password)) {
+        echo "password is empty";
+    }
+}
+
+function sanitizeInput($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function is_username_empty($username) {
+    return empty($username);
+}
+
+function is_password_empty($password) {
+    return empty($password);
+}
