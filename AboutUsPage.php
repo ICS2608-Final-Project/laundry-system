eaa5550d96870b0a735b2853dd92808495e14094
MJFSOL <151334171+MJFSOL@users.noreply.github.com>
2023-12-06 18:21:37 +0800

    pa check Aboutpage

---
+++ AboutUsPage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>AboutUsPage</title>
+</head>
+<body>
+    <div class="hero">
+        <div class="container-lg">
+                <div class="row">
+                    <div class="col">
+                        <div class="grp1">
+                            <p class="Reasons">Reasons to Book
+                                <br> with Love Hon
+                            </p>
+                            <button class="btn1">Book Now</button>
+                        </div>    
+                    </div>
+                    <div class="col">
+                        <div class="grp2">
+                            <h1>How it Work</h1>
+                            <p class="Make">Make it a Fun Experience</p>   
+                            <br>                
+                            <ol class = "num">
+                                <li>Book Laundry Pick-Up</li>
+                                <li>We pick-up your Clothes</li>
+                                <li>We Clean your Clothes</li>
+                                <li>Pay Online / Cash on Delivery(COD)</li>
+                                <li>We Deliver fresh and Clean Clothes</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
+<style>
+    <?php include "AboutUsPage.css"?>
+</style>
