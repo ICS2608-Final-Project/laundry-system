1622ebfffd2a1f99e9555a70935ca24908822fcc
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-07 14:21:25 +0800

    fixed navbar component

--- template/header.php
+++ template/header.php
@@ -4,10 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="scss/header.scss" />
-    <link rel="stylesheet" href="scss/_typography.scss" />
-    <link rel="stylesheet" href="scss/main.scss" />
-    <title><?php $page_title = isset($page_title) ? $page_title : 'Home';echo $page_title ?></title>
+    <link rel="stylesheet" href="css/main.css" />
+    <title><?php $page_title = isset($page_title) ? $page_title : 'Home'; echo $page_title ?></title>
 </head>
     <body>
         <header class="navbar">
