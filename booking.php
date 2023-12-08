0cf3786a58cde80bb69125d7ae4a3013b592cd2f
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-09 06:41:48 +0800

    added booking form

--- booking.php
+++ booking.php
@@ -6,12 +6,53 @@
     require_once "template/header.php";
     require_once 'classes/ServicesModel.php';
     $progress_status = [];
+    $services = (new ServicesModel())->fetch_services();
 ?>
 <main class="booking-section">
-    <form action="">
+    <form action="" class="booking-form">
         <div class="">
             <?php require_once 'template/progressbar.component.php'; ?>
         </div>
+        <select name="service" id="service">
+            <option selected disabled>-- Select a Service --</option>
+            <?php 
+            foreach($services as $service) {
+                echo "<option value=". $service['service_id'] . ">" . ucwords($service['service_name']) . "</option>";
+            }
+            ?>
+        </select>
+        <div class="service-info">
+            <p class="service-price" id="servicePrice"></p>
+            <p class="service-description" id="serviceDescription"></p>
+        </div>
+        <button type="submit">Continue</button>
     </form>
+    <script>
+        const services = [
+            <?php 
+            foreach($services as $service) {
+                echo "{";
+                echo "serviceId: ". $service['service_id'] . ", ";
+                echo "serviceName: '". $service['service_name'] . "', ";
+                echo "serviceDescription: '". $service['service_description'] . "', ";
+                echo "servicePrice: '". $service['service_price'] . "', ";
+                echo "},";
+            }
+            ?>
+        ];
+
+        const service = document.getElementById('service');
+
+        service.addEventListener('change', () => {
+            services.forEach((serviceInfo) => {
+                const servicePrice = document.getElementById('servicePrice');
+                const serviceDescription = document.getElementById('serviceDescription');
+                if (serviceInfo['serviceId'] == service.value) {
+                    servicePrice.innerHTML = "P" +serviceInfo['servicePrice'];
+                    serviceDescription.innerHTML = serviceInfo['serviceDescription'];
+                }
+            });
+        });
+    </script>
 </main>
 <?php include_once 'template/footer.php' ?>
