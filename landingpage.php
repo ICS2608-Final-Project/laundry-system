3de1d86e5707e7b83268d601edc63f8f531b8a31
kmocorro24 <kmocorro24@gmail.com>
2023-12-08 16:27:35 +0800

    updated landingpage.php ( contact us form component nalang natitira)

--- landingpage.php
+++ landingpage.php
@@ -1,34 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Landing Page</title>
     <link rel="stylesheet" href="scss/_landing.scss">
 </head>
+
 <body>
-<header class="navbar">
+    <header class="navbar">
         <img src="assets/Logo.svg" alt="Logo" class="navlogo" />
         <nav class="navright">
             <ul class="navlinks">
                 <li><a href="">Home</a></li>
                 <li><a href="">Pricing</a></li>
                 <li><a href="">About Us</a></li>
+                <button class="book-now-btn" name="Booknow">Book Now</button>
             </ul>
-            <a class="book-now-btn" href="">Book Now
-            </a>
         </nav>
     </header>
 
     <div class="container1">
         <img src="assets/LaundryWallpaper.svg" alt="background" class="bg">
         <div class="desc">
-           <h1> Spice up your wardrobe</h1>
-           <h1> on laundry day</h1>
-           <a class="book-now-btn" href="">Book Now</a>
+            <h1> Spice up your wardrobe
+                <br> on laundry day
+            </h1>
+            <button class="book-now-btn" name="Booknow">Book Now</button>
         </div>
     </div>
 
+    <div class="aboutpage">
+        <div class="row">
+            <div class="col">
+                <div class="grp1">
+                    <p class="Reasons">Reasons to Book
+                        <br> with Love Hon
+                    </p>
+                    <button class="book-now-btn" name="Booknow">Book Now</button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="grp2">
+                    <h1>How it Work</h1>
+                    <p class="Make">Make it a Fun Experience</p>
+                    <br>
+                    <ol class="num">
+                        <li>Book Laundry Pick-Up</li>
+                        <li>We pick-up your Clothes</li>
+                        <li>We Clean your Clothes</li>
+                        <li>Pay Online / Cash on Delivery(COD)</li>
+                        <li>We Deliver fresh and Clean Clothes</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <footer>
         <div class="footer-left">
@@ -51,6 +80,7 @@
             <p class="footer-legal">&copy; 2023 LoveHon. All rights reserved</p>
         </div>
     </footer>
-    
+
 </body>
+
 </html>
