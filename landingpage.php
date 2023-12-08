a7fc1a39cc428b336a9144cf68628adad2c11167
kmocorro24 <kmocorro24@gmail.com>
2023-12-08 10:00:20 +0800

    created landingpage.php (not yet done)

---
+++ landingpage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="scss/_landing.scss">
+</head>
+<body>
+<header class="navbar">
+        <img src="assets/Logo.svg" alt="Logo" class="navlogo" />
+        <nav class="navright">
+            <ul class="navlinks">
+                <li><a href="">Home</a></li>
+                <li><a href="">Pricing</a></li>
+                <li><a href="">About Us</a></li>
+            </ul>
+            <a class="book-now-btn" href="">Book Now
+            </a>
+        </nav>
+    </header>
+
+    <div class="container1">
+        <img src="assets/LaundryWallpaper.svg" alt="background" class="bg">
+        <div class="desc">
+           <h1> Spice up your wardrobe</h1>
+           <h1> on laundry day</h1>
+           <a class="book-now-btn" href="">Book Now</a>
+        </div>
+    </div>
+
+
+    <footer>
+        <div class="footer-left">
+            <img src="assets/FooterLogo.svg" alt="FooterLogo" class="footer-left">
+        </div>
+
+        <div class="footer-center">
+            <p class="center-time">Open Daily 8am - 9pm</p>
+            <p class="center-number">63+ 0947-965-4342</p>
+            <p class="center-loc">Unit 5. Montville place carlos st. <br />Brgy. San Bartolome, Quezon City</p>
+        </div>
+
+        <div class="footer-right">
+            <ul class="footer-links">
+                <li class="li-label">Pages</li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Pricing</a></li>
+                <li><a href="">About Us</a></li>
+            </ul>
+            <p class="footer-legal">&copy; 2023 LoveHon. All rights reserved</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
