0da90c7341ab584af3dd39a07b0a09c7b0bd6d3a
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-14 22:05:01 +0800

    finished styling pickup details

--- book.pickup.php
+++ book.pickup.php
@@ -12,36 +12,40 @@ $progress_status = ['pickup'];
 ?>
 <main class="booking-section">
     <form action="" class="pickup-form" method="post">
-        <div class="name">
-            <h4>Name</h4>
-            <div class="name-input">
-                <input type="text" name="first_name" id="first_name" placeholder="First Name" autocomplete="off" required>
-                <input type="text" name="last_name" id="last_name" placeholder="Last Name" autocomplete="off" required>
+        <?php require_once 'template/progressbar.component.php' ?>
+        
+        <div class="pickup-inputs">
+            <div class="name">
+                <h4>Name</h4>
+                <div class="name-input">
+                    <input type="text" name="first_name" id="first_name" placeholder="First Name" autocomplete="off" required>
+                    <input type="text" name="last_name" id="last_name" placeholder="Last Name" autocomplete="off" required>
+                </div>
             </div>
+            <div class="mobile-input form-input">
+                <label for="mobile_number">Mobile No.</label>
+                <input type="text" name="mobile_number" id="mobile_number" placeholder="+63 XXX-XXX-XXXX" autocomplete="off" required>
+            </div>
+            <div class="email-input form-input">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+            </div>
+            <div class="payment_method-input form-input">
+                <select name="payment_method" id="payment_method" required>
+                    <option value="" selected disabled>-- Select Payment Method --</option>
+                    <?php 
+                    require_once 'classes/PaymentMethodModel.php';
+                    foreach((new PaymentMethodModel())->fetch_payment_methods() as $payment_method) {
+                        echo "<option value='". $payment_method['payment_method_id'] ."'>". ucwords($payment_method['payment_method_name'])."</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="location-input form-input">
+                <textarea name="Pickup and Delivery Address" id="" rows="3" placeholder="Enter pickup location"></textarea>
+            </div>
+            <button type="submit">Continue</button>
         </div>
-        <div class="">
-            <label for="mobile_number">Mobile No.</label>
-            <input type="text" name="mobile_number" id="mobile_number" placeholder="+63 XXX-XXX-XXXX" autocomplete="off" required>
-        </div>
-        <div class="">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" required>
-        </div>
-        <div class="">
-            <select name="payment_method" id="payment_method" required>
-                <option value="" selected disabled>-- Select Payment Method --</option>
-                <?php 
-                require_once 'classes/PaymentMethodModel.php';
-                foreach((new PaymentMethodModel())->fetch_payment_methods() as $payment_method) {
-                    echo "<option value='". $payment_method['payment_method_id'] ."'>". ucwords($payment_method['payment_method_name'])."</option>";
-                }
-                ?>
-            </select>
-        </div>
-        <div class="">
-            <textarea name="Pickup and Delivery Address" id="" rows="3" placeholder="Enter pickup location"></textarea>
-        </div>
-        <button type="submit">Continue</button>
     </form>
     </script>
 </main>
