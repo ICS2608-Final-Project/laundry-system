84898cfff4b0f0a68c70faa28351e32538f466a5
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-12-14 21:21:56 +0800

    added styling for form input and textarea

--- book.pickup.php
+++ book.pickup.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'config/session.config.php';
-$_SESSION['service_id'];
 
 $page_title = "Love Hon: Pickup details";
 $current_page = 'booking';
@@ -12,24 +11,24 @@ require_once "template/header.php";
 $progress_status = ['pickup'];
 ?>
 <main class="booking-section">
-    <form action="" class="booking-form" method="post">
+    <form action="" class="pickup-form" method="post">
         <div class="name">
             <h4>Name</h4>
             <div class="name-input">
-                <input type="text" name="first_name" id="first_name" placeholder="First Name">
-                <input type="text" name="last_name" id="last_name" placeholder="Last Name">
+                <input type="text" name="first_name" id="first_name" placeholder="First Name" autocomplete="off" required>
+                <input type="text" name="last_name" id="last_name" placeholder="Last Name" autocomplete="off" required>
             </div>
         </div>
         <div class="">
             <label for="mobile_number">Mobile No.</label>
-            <input type="text" name="mobile_number" id="mobile_number" placeholder="+63 XXX-XXX-XXXX">
+            <input type="text" name="mobile_number" id="mobile_number" placeholder="+63 XXX-XXX-XXXX" autocomplete="off" required>
         </div>
         <div class="">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email">
+            <input type="email" name="email" id="email" placeholder="Email" required>
         </div>
         <div class="">
-            <select name="payment_method" id="payment_method">
+            <select name="payment_method" id="payment_method" required>
                 <option value="" selected disabled>-- Select Payment Method --</option>
                 <?php 
                 require_once 'classes/PaymentMethodModel.php';
